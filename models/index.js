2e4cc9cd0ba142d7563ff3ee3913f0a2ab0ed115
Brandon E <brandonelliott0594@gmail.com>
2022-08-03 20:17:50 -0600

    added models

--- models/index.js
+++ models/index.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+const User = require("./UserData");
+const Ingredient = require("./Ingredient");
+const Recipe = require("./Recipe");
+
+Recipe.belongsTo(Ingredient, {
+  through: {
+    model: Recipe,
+    unique: false,
+  },
+  as: "planned_recipes",
+});
+
+User.hasMany(Recipe, {
+  through: {
+    Model: Recipe,
+    unique: false,
+  },
+  as: "user_recipes",
+});
+
+module.exports = { User, Ingredient, Recipe };
