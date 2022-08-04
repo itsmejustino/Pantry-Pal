531d9ac1f160842c7a6a957b74f04e53be5c3e87
jdog <thuz3rfr13ndly@gmail.com>
2022-08-03 21:11:07 -0600

    async fetch

--- controllers/script.js
+++ controllers/script.js
@@ -1,16 +1,35 @@
-const fetch = require('node-fetch');
-// const userSearch = 
-const url = 'https://edamam-recipe-search.p.rapidapi.com/search?q=chicken&to=2';
+const fetch = require("node-fetch");
+// const userSearch =
 
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'eca86d4e2fmsh158280de98a7ef7p14ea91jsn088c0f0e454d',
-    'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
+const fetchedData = async () => {
+  try {
+    const url =
+      "https://edamam-recipe-search.p.rapidapi.com/search?q=chicken&to=2";
+
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "eca86d4e2fmsh158280de98a7ef7p14ea91jsn088c0f0e454d",
+        "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
+      },
+    };
+
+    const response = await fetch(url, options);
+
+    const data = await response.json();
+
+    const results = await data.hits;
+    const cautionResults = await data.hits[1].recipe.cautions;
+    //returns recipe results and the caution for the 2nd recipe
+    // console.log(results);
+    // console.log(cautionResults);
+    return results, cautionResults;
+  } catch (err) {
+    (err) => console.error("error:" + err);
   }
 };
 
-fetch(url, options)
-	.then(res => res.json())
-	.then(json => console.log(json.hits))
-	.catch(err => console.error('error:' + err));
+const theData = fetchedData();
+console.log(theData);
+
+module.export = fetchedData;
