a87c068d414892f330337883c32fdbae1b14b07e
Justino <justindortiz@me.com>
2022-08-07 22:33:04 -0600

    reinsert homeroutes file

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,66 @@
+// const router = require("express").Router();
+// const { User } = require("../models");
+// const withAuth = require("../utils/auth");
+
+// router.get("/", withAuth, async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       attributes: { exclude: ["password"] },
+//       order: [["name", "ASC"]],
+//     });
+
+//     const users = userData.map((project) => project.get({ plain: true }));
+
+//     res.render("homepage", {
+//       users,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get("/login", (req, res) => {
+//   if (req.session.logged_in) {
+//     res.redirect("/");
+//     return;
+//   }
+
+//   res.render("login");
+// });
+
+// module.exports = router;
+
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
