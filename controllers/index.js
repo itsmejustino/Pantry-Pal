e00cc5e60c0c4f0d1866af372a95d183fb98555f
Brandon E <brandonelliott0594@gmail.com>
2022-08-08 18:29:00 -0600

    Stash

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 
 const apiRoute = require("./api");
 const homeRoute = require("./homeRoutes");
+const signupRoute = require("./signupRoute.js");
 
 // const profileRoute = require("./api/userprofile");
 
 router.use("/", homeRoute);
 router.use("/api", apiRoute);
+router.use("/signup", signupRoute);
 
 // router.use("/userprofile", profileRoute);
 
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,15 +1,13 @@
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document
-    .querySelector("#name-signup")
-    .value.trim()
-    .toLowerCase();
+  const name = document.querySelector("#name-signup").value.trim();
+
   const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
   if (name && email && password) {
-    const response = await fetch("api/users/signup", {
+    const response = await fetch("api/signup", {
       method: "POST",
       body: JSON.stringify({ name, email, password }),
       headers: { "Content-Type": "application/json" },
