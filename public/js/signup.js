3c76d9bdabd7691bd5d525ce4badc5721506c6ad
Quy Nguyen <qnguyen303@gmail.com>
2022-08-09 23:51:16 -0600

    login and logout

--- public/js/signup.js
+++ public/js/signup.js
@@ -21,5 +21,5 @@ const signupFormHandler = async (event) => {
 };
 
 document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
+  .querySelector("#signup-btn")
+  .addEventListener("click", signupFormHandler);
